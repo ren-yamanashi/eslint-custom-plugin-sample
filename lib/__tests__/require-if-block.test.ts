3bb77fc4683bcbebacbadaf5e16dd50aceb419fc
ren-yamanashi <pinokionigg@icloud.com>
2024-12-23 00:13:49 +0900

    01. add test

---
+++ lib/__tests__/require-if-block.test.ts
@@ -0,0 +1,16 @@
+import { RuleTester } from "eslint";
+import { requireIfBlock } from "../require-if-block.mjs";
+
+const ruleTester = new RuleTester({
+  languageOptions: { ecmaVersion: "latest", sourceType: "module" },
+});
+
+ruleTester.run("require-if-block", requireIfBlock, {
+  valid: [],
+  invalid: [
+    {
+      code: `if (true) console.log("true");`,
+      errors: [{ messageId: "requireIfBlock" }],
+    },
+  ],
+});
